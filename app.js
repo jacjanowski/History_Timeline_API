483baef41c14a08a394ccc03a9c28525859dca3e
Jacob Janowski <60745870+jacjanowski@users.noreply.github.com>
2021-05-20 10:49:35 -0500

    hard code three objects to pass

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
-var express    = require("express"),
+var express = require("express"),
     bodyParser = require("body-parser"),
-    request	   = require("request");
+    request = require("request");
 
 
 var PORT = process.env.PORT || 3000;
@@ -10,24 +10,37 @@ var app = express();
 
 const URL = 'https://byabbe.se/on-this-day/1/3/events.json'
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public/'));
 
-app.get("/", function(req,res){
+app.get("/", function (req, res) {
 
-    request(URL, function(error, response, body){
-        if(!error && response.statusCode == 200){
+    request(URL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             console.log("----------------------------------")
+
+
             var dataObject = [
                 {
                     date: data.date,
                     year: data.events[0].year,
                     description: data.events[0].description
+                },
+                {
+                    date: data.date,
+                    year: data.events[1].year,
+                    description: data.events[1].description
+                },
+                {
+                    date: data.date,
+                    year: data.events[2].year,
+                    description: data.events[2].description
                 }
-            ]
-            res.render("show",{dataObject: dataObject});
+            ];
+
+            res.render("show", { dataObject: dataObject });
         }
     })
 
@@ -45,6 +58,6 @@ app.get("/", function(req,res){
 
 
 
-app.listen(PORT,function(){
-    console.log('server successfully started on port '+PORT);
+app.listen(PORT, function () {
+    console.log('server successfully started on port ' + PORT);
 });
