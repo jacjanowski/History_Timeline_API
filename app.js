8433e406224fc6e5a2ce24df2ceb6f52690becda
jacjanowski <jjanow7@uic.edu>
2021-06-04 09:41:05 -0500

    add deaths and births pages. working properly.

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.get("/", function (req, res) {
 
 
 
-app.post('/on-this-day', function (req, res) {
+app.post('/on-this-day/events', function (req, res) {
 
     var URL = 'https://byabbe.se/on-this-day/' + req.body.month + "/" + req.body.day + "/events.json";
     console.log("URL is: " + URL);
@@ -42,19 +42,87 @@ app.post('/on-this-day', function (req, res) {
                     description: data.events[i].description,
                     amount: data.events[i].wikipedia.length
                 });
-                for(var j = 0; j < data.events[i].wikipedia.length; j++){
+                for (var j = 0; j < data.events[i].wikipedia.length; j++) {
                     sources.push({
                         title: data.events[i].wikipedia[j].title,
                         wikipedia: data.events[i].wikipedia[j].wikipedia
                     });
                 }
-                
+
 
             }
 
             res.render("show", { dataObject: arrayOfEvents, sourceObject: sources });
         }
     })
+})
+
+app.post('/on-this-day/births', function (req, res) {
+    var URL = 'https://byabbe.se/on-this-day/' + req.body.month + "/" + req.body.day + "/births.json";
+    console.log("URL is: " + URL);
+
+    request(URL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+            var arrayOfEvents = [];
+            var sources = [];
+            var data = JSON.parse(body);
+            var eventLength = data.births.length;
+            for (var i = 0; i < eventLength; i++) {
+                arrayOfEvents.push({
+                    date: data.date,
+                    year: data.births[i].year,
+                    description: data.births[i].description,
+                    amount: data.births[i].wikipedia.length
+                });
+                for (var j = 0; j < data.births[i].wikipedia.length; j++) {
+                    sources.push({
+                        title: data.births[i].wikipedia[j].title,
+                        wikipedia: data.births[i].wikipedia[j].wikipedia
+                    });
+                }
+
+
+            }
+
+            res.render("births", { dataObject: arrayOfEvents, sourceObject: sources });
+        }
+    })
+
+})
+
+app.post('/on-this-day/deaths', function (req, res) {
+    var URL = 'https://byabbe.se/on-this-day/' + req.body.month + "/" + req.body.day + "/deaths.json";
+    console.log("URL is: " + URL);
+
+    request(URL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+            var arrayOfEvents = [];
+            var sources = [];
+            var data = JSON.parse(body);
+            var eventLength = data.deaths.length;
+            for (var i = 0; i < eventLength; i++) {
+                arrayOfEvents.push({
+                    date: data.date,
+                    year: data.deaths[i].year,
+                    description: data.deaths[i].description,
+                    amount: data.deaths[i].wikipedia.length
+                });
+                for (var j = 0; j < data.deaths[i].wikipedia.length; j++) {
+                    sources.push({
+                        title: data.deaths[i].wikipedia[j].title,
+                        wikipedia: data.deaths[i].wikipedia[j].wikipedia
+                    });
+                }
+
+
+            }
+
+            res.render("deaths", { dataObject: arrayOfEvents, sourceObject: sources });
+        }
+    })
+
 })
 
 app.get('/on-this-day/:month/:day', function (req, res) {
